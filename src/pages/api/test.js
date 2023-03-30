6cc0f6aa3982f6700ed1b1130f1d7d0af0fe008d
TeamCorpora <somoscorporaweb@gmail.com>
2023-03-30 02:03:46 -0300

    test

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -6,12 +6,16 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function handler(req, res) {
-  const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      { role: "system", content: "Sos un botazo bro, equipo de Corpora." },
-      { role: "user", content: "De qué equipo formas parte?" },
-    ],
-  });
-  res.status(200).json(completion.data.choices[0].message.content);
+  try {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "system", content: "Sos un botazo bro, equipo de Corpora." },
+        { role: "user", content: "De qué equipo formas parte?" },
+      ],
+    });
+    res.status(200).json(completion.data.choices[0].message.content);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
