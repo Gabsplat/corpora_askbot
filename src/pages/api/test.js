b7e8290fd31d13ec0b2097007247bca8c4d0595a
TeamCorpora <somoscorporaweb@gmail.com>
2023-05-02 02:15:48 -0300

    test

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -1,7 +1,22 @@
-import { getSheetsData } from "@/server/utils";
+const { Configuration, OpenAIApi } = require("openai");
+
+// Open ai
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const EMBEDDING_MODEL = "text-embedding-ada-002";
 
 export default async function handler(req, res) {
-  const data = await getSheetsData();
-  console.log("Getting data...");
-  res.status(200).json(data);
+  try {
+    let queryEmbedding = await openai.createEmbedding({
+      model: EMBEDDING_MODEL,
+      input: question,
+    });
+
+    res.status(200).json({ queryEmbedding });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
 }
